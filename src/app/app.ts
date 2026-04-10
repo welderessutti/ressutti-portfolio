c4e37848d22053cf155566f5607d8615f1b10f67
welderessutti <welderessutti@gmail.com>
2026-04-10 18:21:14 -0300

    footer built and color changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Home } from './features/home/home';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home],
+  imports: [RouterOutlet, Header, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
