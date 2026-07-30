df0a93a75b6e4eb7286a6b0cae04f5da8587a723
welderessutti <welderessutti@gmail.com>
2026-07-30 15:26:19 -0300

    Imporves Home projects and stacks cards layout

--- src/app/features/home/sections/projects/project-card/project-card.ts
+++ src/app/features/home/sections/projects/project-card/project-card.ts
@@ -1,12 +1,22 @@
-import { Component, input } from '@angular/core';
-import { Project } from '../../../../../shared/models/project.model'
+import { Component, input, DOCUMENT, inject } from '@angular/core';
+import { Project } from '../../../../../shared/models/project.model';
+import { RouterLink } from '@angular/router';
+import { Locale } from '../../../../../shared/i18n/locales';
+import { ROUTES } from '../../../../../shared/i18n/routes';
 
 @Component({
   selector: 'app-project-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './project-card.html',
   styleUrl: './project-card.css',
 })
 export class ProjectCard {
+  private readonly document = inject(DOCUMENT);
+  protected readonly currentLocale = this.currentLocaleHtml;
+  protected readonly projectsPagePath = ROUTES.projects[this.currentLocale];
   public readonly project = input.required<Project>();
+
+  private get currentLocaleHtml(): Locale {
+    return this.document.documentElement.lang as Locale;
+  }
 }
